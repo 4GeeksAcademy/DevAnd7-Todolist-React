9b70bdd94dd7049c86a887f26b8b490ae28357c1
Andri Arrieta <andriarrieta7@gmail.com>
2026-01-13 20:16:52 +0000

    todos con react

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,8 +4,39 @@ import React, { useEffect, useState } from "react";
 //create your first component
 const Home = () => {
 
+	useEffect(() => {
+		fetch('https://playground.4geeks.com/todo/users/DevAnd7')
+			.then(response => {
+				return response.json();
+			})
+			.then(data => {
+				setActivity([...data.todos])
+				console.log(data)
+			})
+			.catch(error => {
+				console.log('Oh No! There was a problem: \n', error);
+			});
+
+	}, []);
+
+	//POST
+	const createPost = (task) => {
+
+		fetch('https://playground.4geeks.com/todo/todos/DevAnd7', {
+			method: 'POST',
+			body: JSON.stringify({
+				label: task.label,
+				is_done: false
+			}),
+			headers: {
+				'Content-Type': 'application/json; charset=UTF-8',
+			},
+		}).then((response) => response.json())
+	}
+
 
 	const [task, setTask] = useState(null);
+
 
 	const updateTask = (key, value) => {
 		const newTask = {
@@ -19,7 +50,7 @@ const Home = () => {
 	const [activity, setActivity] = useState([])
 
 	const addActivity = () => {
-		if (!task || !task.action) {
+		if (!task || !task.label) {
 			alert('Complete the task')
 			return
 		}
@@ -27,15 +58,39 @@ const Home = () => {
 			...activity,
 			task
 		]
+		createPost(task)
 		setActivity(newActivity);
 		setTask(null)
 	}
 
+	//DELETE
 
 	const deleteActivity = (index) => {
+		const deleteTask = activity[index]
+
 		const newActivity = activity.filter((_, i) => i !== index);
 		setActivity(newActivity);
+		eraseTask(deleteTask.id)
 	};
+
+	const eraseTask = (id) => {
+		fetch(`https://playground.4geeks.com/todo/todos/${id}`,
+			{
+				method: 'DELETE'
+			}
+		).then((response) => response.json())
+	}
+
+
+	const resetList = () => {
+		activity.forEach((tarea) => {
+			eraseTask(tarea.id)
+		}
+		); setActivity([])
+	}
+
+
+
 
 	return (
 		<div className="text-center fs-4">
@@ -43,9 +98,9 @@ const Home = () => {
 			<h1 className="text-center mt-5">Aplicación de Todolist usando React y Fetch</h1>
 
 
-			<input type="text" name="action" placeholder="Write the task"
-				value={task ? task.action : ''}
-				onChange={(evt) => updateTask('action', evt.target.value)}
+			<input type="text" name="label" placeholder="Write the task"
+				value={task ? task.label : ''}
+				onChange={(evt) => updateTask('label', evt.target.value)}
 			/>
 			<div className="btn btn-warning"
 				onClick={() => addActivity()}>
@@ -53,13 +108,13 @@ const Home = () => {
 			</div>
 
 			<div className="container">
-			<hr />
+				<hr />
 				{
 					activity.map((item, index) => (
 						<div className="d-flex justify-content-between gap-3">
 							<div className="d-flex flex-grow-1 justify-content-between">
 
-								<p>Task: {item.action}</p>
+								<p>Task: {item.label}</p>
 
 							</div>
 
@@ -76,9 +131,10 @@ const Home = () => {
 				}
 
 				<hr />
-				<div className="btn btn-success">Button Reset</div>
+				<div className="btn btn-success" onClick={() => resetList()}>
+					Button Reset
+				</div>
 			</div>
-
 
 		</div>
 	);
